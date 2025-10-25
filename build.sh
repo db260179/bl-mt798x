6f2210bf5a2555bf1441585b595963f733eed7a6
David Bentham <db260179@gmail.com>
2025-10-25 15:27:10 +0100

    mt7981: Add comfast cf-e393ax and cf-e593ax support
    Add note on required host packages to build
    
    Signed-off-by: David Bentham <db260179@gmail.com>

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ if [ -z "$SOC" ] || [ -z "$BOARD" ]; then
 	echo "eg: SOC=mt7981 BOARD=360t7 $0"
 	echo "eg: SOC=mt7981 BOARD=wr30u MULTI_LAYOUT=1 $0"
 	echo "eg: SOC=mt7981 BOARD=cmcc_rax3000m-emmc $0"
+	echo "eg: SOC=mt7981 BOARD=comfast_cf-e393ax $0"
+	echo "eg: SOC=mt7981 BOARD=comfast_cf-e593ax $0"
 	echo "eg: SOC=mt7986 BOARD=redmi_ax6000 MULTI_LAYOUT=1 $0"
 	echo "eg: SOC=mt7986 BOARD=jdcloud_re-cp-03 $0"
 	exit 1
